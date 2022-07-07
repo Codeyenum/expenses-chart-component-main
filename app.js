d6fe357ab2a12c758e31de0f534827bba040c340
Abanum Olise <abanum00@gmail.com>
2022-07-07 19:34:05 +0100

    Hover and Click Listeners

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ let bars = document.querySelectorAll(".bar");
 let barColumns = document.querySelectorAll(".bar_column");
 let balanceBox = document.querySelectorAll(".balance");
 
-// for (let bar of bars) {   
-//    bar.addEventListener("mouseenter", () => {
-//       bar.previousElementSibling.classList.toggle("hide");
-//       let barHeight = bar.clientHeight;
-//       bar.previousElementSibling.style.bottom = `${barHeight + 50}px`;
-//    })
-// }
+for (let bar of bars) {   
+   bar.addEventListener("mouseenter", () => {
+      bar.previousElementSibling.classList.toggle("hide");
+      let barHeight = bar.clientHeight;
+      bar.previousElementSibling.style.bottom = `${barHeight + 50}px`;
+   })
+}
 
 for (let bar of bars) {   
    bar.addEventListener("click", () => {
