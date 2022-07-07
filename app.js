51365bc634d32ca3564d42b07b68de126a19b21a
Abanum Olise <abanum00@gmail.com>
2022-07-07 19:23:14 +0100

    more eventlisteners

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let barColumns = document.querySelectorAll(".bar_column");
 let balanceBox = document.querySelectorAll(".balance");
 
 for (let bar of bars) {   
-   bar.addEventListener("click", "mouseenter", () => {
+   bar.addEventListener("mouseenter", "click", () => {
       bar.previousElementSibling.classList.toggle("hide");
       let barHeight = bar.clientHeight;
       bar.previousElementSibling.style.bottom = `${barHeight + 50}px`;
