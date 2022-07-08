d5ee0ee12b43fece4e331a72db14a7fb3173f1c4
Abanum Olise <93771452+Codeyenum@users.noreply.github.com>
2022-07-08 02:59:05 +0100

    Update app.js

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ for (let bar of bars) {
    })
 }
 
-for (let bar of bars) {
-   bar.addEventListener("click", () => {
-      bar.previousElementSibling.classList.remove("hide");
-      let barHeight = bar.clientHeight;
-      bar.previousElementSibling.style.bottom = `${barHeight + 50}px`;
-   })
-}
+//for (let bar of bars) {
+//   bar.addEventListener("click", () => {
+//      bar.previousElementSibling.classList.remove("hide");
+//      let barHeight = bar.clientHeight;
+//      bar.previousElementSibling.style.bottom = `${barHeight + 50}px`;
+//   })
+//}
 
 for (let bar of bars) {
    bar.addEventListener("mouseleave", () => {
@@ -36,12 +36,12 @@ for (let bar of bars) {
 
 //    console.log(spendingData)
 
-// if (window.innerWidth < 960) {
-//    for (let bar of bars) {
-//       bar.addEventListener("click", () => {
-//          bar.previousElementSibling.classList.toggle("hide");
-//          let barHeight = bar.clientHeight;
-//          bar.previousElementSibling.style.bottom = `${barHeight + 50}px`;
-//       })
-//    }
-// }
+ if (window.innerWidth < 960) {
+    for (let bar of bars) {
+       bar.addEventListener("click", () => {
+          bar.previousElementSibling.classList.toggle("hide");
+          let barHeight = bar.clientHeight;
+          bar.previousElementSibling.style.bottom = `${barHeight + 50}px`;
+       })
+    }
+ }
