9755c37501145bd37de585e093497ced39e1dfa0
Abanum Olise <abanum00@gmail.com>
2022-07-08 02:34:37 +0100

    mobile event listener check

--- app.js
+++ app.js
@@ -2,27 +2,46 @@ let bars = document.querySelectorAll(".bar");
 let barColumns = document.querySelectorAll(".bar_column");
 let balanceBox = document.querySelectorAll(".balance");
 
-for (let bar of bars) {   
-   bar.addEventListener("mouseenter", () => {
-      bar.previousElementSibling.classList.toggle("hide");
-      let barHeight = bar.clientHeight;
-      bar.previousElementSibling.style.bottom = `${barHeight + 50}px`;
-   })
-}
+// for (let bar of bars) {
+//    bar.addEventListener("mouseenter", () => {
+//       bar.previousElementSibling.classList.toggle("hide");
+//       let barHeight = bar.clientHeight;
+//       bar.previousElementSibling.style.bottom = `${barHeight + 50}px`;
+//    })
+// }
 
-for (let bar of bars) {   
-   bar.addEventListener("click", () => {
-      bar.previousElementSibling.classList.toggle("hide");
-      let barHeight = bar.clientHeight;
-      bar.previousElementSibling.style.bottom = `${barHeight + 50}px`;
-   })
-}
+// for (let bar of bars) {
+//    bar.addEventListener("click", () => {
+//       bar.previousElementSibling.classList.toggle("hide");
+//       let barHeight = bar.clientHeight;
+//       bar.previousElementSibling.style.bottom = `${barHeight + 50}px`;
+//    })
+// }
 
-for (let bar of bars) {   
-   bar.addEventListener("mouseleave", () => {
-      bar.previousElementSibling.classList.add("hide");  
-      let barHeight = bar.clientHeight;
-      bar.previousElementSibling.style.bottom = `${barHeight + 50}px`;    
-   })
-}
+// for (let bar of bars) {
+//    bar.addEventListener("mouseleave", () => {
+//       bar.previousElementSibling.classList.add("hide");
+//       let barHeight = bar.clientHeight;
+//       bar.previousElementSibling.style.bottom = `${barHeight + 50}px`;
+//    })
+// }
 
+// fetch("./data.json")
+//    .then(function (resp) {
+//       return resp.json();
+//    })
+//    .then(function (data) {
+//       let spendingData = data;
+//    })
+
+//    console.log(spendingData)
+
+if (window.innerWidth < 960) {
+   for (let bar of bars) {
+      bar.addEventListener("click", () => {
+         bar.previousElementSibling.classList.toggle("hide");
+         let barHeight = bar.clientHeight;
+         bar.previousElementSibling.style.bottom = `${barHeight + 50}px`;
+      })
+   }
+}
