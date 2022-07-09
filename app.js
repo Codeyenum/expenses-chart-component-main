d7cbc8cae9f3330d2e3401a35c782c50f0aec1c5
Abanum Olise <abanum00@gmail.com>
2022-07-09 20:23:03 +0100

    Data Manipulation

--- app.js
+++ app.js
@@ -7,6 +7,23 @@ for (let bar of bars) {
       bar.previousElementSibling.classList.remove("hide");
       let barHeight = bar.clientHeight;
       bar.previousElementSibling.style.bottom = `${barHeight + 50}px`;
+      let day = bar.classList[1];
+      // console.log(day);
+      let getJsonData = async () => {
+         let resp = await fetch("./data.json");
+         let data = await resp.json();
+         return data
+      }
+      
+      getJsonData()
+         .then((dataArray) => {            
+            for (let dataObject of dataArray) {
+               if (dataObject.day === day) {
+                  // console.log(dataObject.amount)
+                  bar.previousElementSibling.firstElementChild.innerText = dataObject.amount;
+               }
+            }
+         })
    })
 }
 
@@ -23,18 +40,59 @@ for (let bar of bars) {
       bar.previousElementSibling.classList.add("hide");
       let barHeight = bar.clientHeight;
       bar.previousElementSibling.style.bottom = `${barHeight + 50}px`;
+
    })
 }
 
+
+// console.dir(bars);
+
 // fetch("./data.json")
-//    .then(function (resp) {
-//       return resp.json();
+//    .then((resp) => {
+//       console.log("RESPONSE:", resp)
+//       return resp.json();      
 //    })
-//    .then(function (data) {
+//    .then((data) => {
 //       let spendingData = data;
+//       console.log(spendingData);
+//    })
+//    .catch((e) => {
+//       console.log("error", e);
 //    })
 
-//    console.log(spendingData)
+// let getJsonData = async () => {
+//    let resp = await fetch("./data.json");
+//    let data = await resp.json();
+//    return data
+// }
+
+// getJsonData()
+//    .then((dataArray) => {
+//       console.log(dataArray);
+//       for (let dataObject in dataArray) {
+//          if (dataObject.day === day) {
+//             console.log(day)
+//          }
+//       }
+//    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // if (window.innerWidth < 960) {
 //    for (let bar of bars) {
