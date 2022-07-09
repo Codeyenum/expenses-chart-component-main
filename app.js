da04b3a198ddb80db1d4c2d356a1006ceaf88630
Abanum Olise <abanum00@gmail.com>
2022-07-09 20:30:43 +0100

    cleanup

--- app.js
+++ app.js
@@ -2,27 +2,7 @@ let bars = document.querySelectorAll(".bar");
 let barColumns = document.querySelectorAll(".bar_column");
 let balanceBox = document.querySelectorAll(".balance");
 
-
-// fetch("./data.json")
-//    .then(function (resp) {
-//       return resp.json();
-//    })
-//    .then(function (data) {
-//       let spendingData = data;
-//    })
-
-//    console.log(spendingData)
-
- if (window.innerWidth < 960) {
-    for (let bar of bars) {
-       bar.addEventListener("click", () => {
-          bar.previousElementSibling.classList.toggle("hide");
-          let barHeight = bar.clientHeight;
-          bar.previousElementSibling.style.bottom = `${barHeight + 50}px`;
-       })
-    }
- } else {
-   for (let bar of bars) {
+for (let bar of bars) {
    bar.addEventListener("mouseenter", () => {
       bar.previousElementSibling.classList.remove("hide");
       let barHeight = bar.clientHeight;
@@ -34,9 +14,9 @@ let balanceBox = document.querySelectorAll(".balance");
          let data = await resp.json();
          return data
       }
-      
+
       getJsonData()
-         .then((dataArray) => {            
+         .then((dataArray) => {
             for (let dataObject of dataArray) {
                if (dataObject.day === day) {
                   // console.log(dataObject.amount)
@@ -47,13 +27,13 @@ let balanceBox = document.querySelectorAll(".balance");
    })
 }
 
-//for (let bar of bars) {
-//   bar.addEventListener("click", () => {
-//      bar.previousElementSibling.classList.remove("hide");
-//      let barHeight = bar.clientHeight;
-//      bar.previousElementSibling.style.bottom = `${barHeight + 50}px`;
-//   })
-//}
+for (let bar of bars) {
+   bar.addEventListener("click", () => {
+      bar.previousElementSibling.classList.remove("hide");
+      let barHeight = bar.clientHeight;
+      bar.previousElementSibling.style.bottom = `${barHeight + 50}px`;
+   })
+}
 
 for (let bar of bars) {
    bar.addEventListener("mouseleave", () => {
@@ -63,4 +43,63 @@ for (let bar of bars) {
 
    })
 }
-}
+
+
+// console.dir(bars);
+
+// fetch("./data.json")
+//    .then((resp) => {
+//       console.log("RESPONSE:", resp)
+//       return resp.json();
+//    })
+//    .then((data) => {
+//       let spendingData = data;
+//       console.log(spendingData);
+//    })
+//    .catch((e) => {
+//       console.log("error", e);
+//    })
+
+// let getJsonData = async () => {
+//    let resp = await fetch("./data.json");
+//    let data = await resp.json();
+//    return data
+// }
+
+// getJsonData()
+//    .then((dataArray) => {
+//       console.log(dataArray);
+//       for (let dataObject in dataArray) {
+//          if (dataObject.day === day) {
+//             console.log(day)
+//          }
+//       }
+//    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if (window.innerWidth < 960) {
+//    for (let bar of bars) {
+//       bar.addEventListener("click", () => {
+//          bar.previousElementSibling.classList.toggle("hide");
+//          let barHeight = bar.clientHeight;
+//          bar.previousElementSibling.style.bottom = `${barHeight + 50}px`;
+//       })
+//    }
+// }
